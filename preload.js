9ca29fbafb6c4d04ecd24eb892754c6298bb487e
addaitech <addaitech@gmail.com>
2022-08-10 13:48:59 -0400

    lounger 0.1 thelounge.chat front end.
    Signed-off-by: addaitech <addaitech@gmail.com>

---
+++ preload.js
@@ -0,0 +1,7 @@
+const { Menu, BrowserWindow, webContents, getCurrentWindow } = require('@electron/remote');
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system')
+})
